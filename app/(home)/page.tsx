8eff1463280c993ecb3e061f873c704d5b5c69d7
Jefferson Sousa <jefferson@eprojts.com.br>
2024-02-12 15:18:58 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import {format} from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <h2 className="text-lg font-bold">Olá, Miguel!</h2>
+      <p className="capitalize text-sm">{format(new Date(), "EEEE', 'dd 'de 'MMMM' de 'yyyy", {
+        locale: ptBR
+      })}</p>
     </div>
   );
 }
