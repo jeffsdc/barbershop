1f8f1ef977afb07e75b22ee6fdf2901ae76945d4
Jefferson Sousa <jefferson@eprojts.com.br>
2024-02-16 00:14:08 -0300

    feat: add initial BarbershopItem component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,12 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarberShopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany()
+
   return (
     <div>
       <Header/>
@@ -21,9 +25,28 @@ export default function Home() {
         <Search/>
       </div>
       <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
         <BookingItem />
       </div>
+      <div className="mt-6">
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">Recomendados</h2>
+        
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+      </div>
+      <div className="mt-6">
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">Populares</h2>
+        
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+      </div>
+
     </div>
   );
 }
