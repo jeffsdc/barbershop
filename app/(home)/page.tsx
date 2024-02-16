d0f435d847331cfdf1e6485be98059b976be4047
Jefferson Sousa <jefferson@eprojts.com.br>
2024-02-16 15:24:02 -0300

    add initial Footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarberShopItem from "./_components/barbershop-item";
+import Footer from "../_components/footer";
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany()
@@ -40,13 +41,12 @@ export default async function Home() {
       <div className="mt-6">
         <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">Populares</h2>
         
-        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        <div className="flex px-5 gap-4 overflow-x-auto mb-[4.5rem] [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
           ))}
         </div>
       </div>
-
     </div>
   );
 }
---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return ( 
+    <div className="bg-secondary py-6 px-5">
+      <p className="text-gray-400 font-xs opacity-75">© 2023 Copyright <b>FSW Barber</b></p>
+    </div>
+   );
+}
+ 
+export default Footer;
