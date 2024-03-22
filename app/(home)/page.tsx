53011505f63a36801398f81a39f8ef3d1b45de4d
jeffsdc <jsousac12@gmail.com>
2024-03-22 11:40:51 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import {format} from "date-fns";
+import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
@@ -7,42 +7,48 @@ import { db } from "../_lib/prisma";
 import BarberShopItem from "./_components/barbershop-item";
 
 export default async function Home() {
-  const barbershops = await db.barbershop.findMany()
+  const barbershops = await db.barbershop.findMany();
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="px-5 pt-5">
         <h2 className="text-lg font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE', 'dd 'de 'MMMM' de 'yyyy", {
-            locale: ptBR
+            locale: ptBR,
           })}
         </p>
       </div>
 
       <div className="px-5 mt-6">
-        <Search/>
+        <Search />
       </div>
       <div className="px-5 mt-6">
-        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-        <BookingItem />
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+        {/* <BookingItem /> */}
       </div>
       <div className="mt-6">
-        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">Recomendados</h2>
-        
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
       <div className="mt-6">
-        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">Populares</h2>
-        
+        <h2 className="px-5 mb-3 text-xs uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+
         <div className="flex px-5 gap-4 overflow-x-auto mb-[4.5rem] [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,35 +1,59 @@
+import { Booking, Prisma } from "@prisma/client";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
+import { format, isPast } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { isFuture } from "date-fns/isFuture";
 
-const BookingItem = () => {
-  return ( 
+interface BookingListProps {
+  booking: Prisma.BookingGetPayload<{
+    include: {
+      service: true;
+      barbershop: true;
+    };
+  }>;
+}
+
+const BookingItem = ({ booking }: BookingListProps) => {
+  const isBookingConfirmed = isFuture(booking.date);
+
+  return (
     <div>
       <Card>
-        <CardContent className="p-5 flex justify-between py-0">
-          <div className="flex flex-col gap-2 py-5">
-            <Badge className="bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit">Confirmado</Badge>
-            <h2 className="font-bold">Corte de Cabelo</h2>
+        <CardContent className="px-0 flex py-0">
+          <div className="flex flex-col gap-2 py-5 flex-[3] pl-5">
+            <Badge
+              variant={!isBookingConfirmed ? "secondary" : "default"}
+              className="w-fit"
+            >
+              {!isBookingConfirmed ? "Finalizado" : "Confirmado"}
+            </Badge>
+            <h2 className="font-bold">{booking.service.name}</h2>
             <div className="flex items-center gap-2">
               <Avatar className="h-6 w-6">
-                <AvatarImage 
-                  src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"
+                <AvatarImage
+                  src={booking.barbershop.imageUrl}
                   alt="Imagem do Corte de Cabelo"
                 />
                 <AvatarFallback>A</AvatarFallback>
               </Avatar>
-              <h3 className="text-sm">Barbearia Vintage</h3>
+              <h3 className="text-sm">{booking.barbershop.name}</h3>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center border-l border-secondary border-solid px-3">
-            <p className="text-sm">Fevereiro</p>
-            <p className="text-2xl">06</p>
-            <p className="text-sm">09:45</p>
+          <div className="flex flex-col justify-center items-center border-l border-secondary border-solid flex-1">
+            <p className="text-sm capitalize">
+              {format(booking.date, "MMMM", {
+                locale: ptBR,
+              })}
+            </p>
+            <p className="text-2xl">{format(booking.date, "dd")}</p>
+            <p className="text-sm">{format(booking.date, "HH:mm")}</p>
           </div>
         </CardContent>
       </Card>
     </div>
-   );
-}
- 
+  );
+};
+
 export default BookingItem;
